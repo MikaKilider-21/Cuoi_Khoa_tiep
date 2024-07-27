54185dbc011fc902cdad2664a623f0e2bfe92a2b
MikaKilider-21 <nguyenthinhkhangnk21@gmail.com>
2024-07-27 15:50:18 +0700

    Load

---
+++ Giỏ hàng/giohang.js
@@ -0,0 +1,7 @@
+let allProduct=JSON.parse(localStorage.getItem("product"));
+for (let item of allProduct ){
+    let card= document.createElement("div")
+    card.classList.add("card");
+    card.id=items.id;
+    let
+}
